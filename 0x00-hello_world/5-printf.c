1430a81050c369d123945fc7caf5aef07728846f
James0711 <jengreat94@gmail.com>
2023-03-16 15:26:32 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints exactly a new line with the printf function
+ * Return: 0 (successful)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (o);
+}
