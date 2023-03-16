df291bc58d605d029db2e0d29402868beed84385
James0711 <jengreat94@gmail.com>
2023-03-16 16:31:27 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio>
+
+/**
+ * main - A C program that prints exactly with a new line
+ * Return: 0 (successful)
+*/
+
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (0);
+}
