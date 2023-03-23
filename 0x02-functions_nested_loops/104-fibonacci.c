0fe5c7a70a540c1dda10d452ec90fba71ab39fac
James0711 <jengreat94@gmail.com>
2023-03-22 19:10:34 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -32,6 +32,8 @@ int main(void)
 	{
 		printf("%lu", aft1 + (aft2 / l));
 		printf("%lu", aft2 % l);
+		printf(",");
+		printf(" ");
 		aft1 = aft1 + bef1;
 		bef1 = aft1 - bef1;
 		aft2 = aft2 + bef2;
