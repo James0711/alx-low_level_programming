ceceed6271b135909e0cf30732d4bf02deec8f7b
James0711 <jengreat94@gmail.com>
2023-04-12 05:44:31 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -16,7 +16,7 @@ int count_word(char *s)
 
 	for (c = 0; s[c] != '\0'; c++)
 	{
-		if (s[c] == '')
+		if (s[c - 1] == ' ')
 			flag = 0;
 		else if (flag == 0)
 		{
@@ -51,20 +51,20 @@ char **strtow(char *str)
 
 	for (i = 0; i <= len; i++)
 	{
-		if (str[i] == '' || str[i] == '\0')
+		if (str[i] == ' ' || str[i - 1] == '\0')
 		{
 			if (c)
 			{
 				end = i;
-				tmp = (char *) malloc((c = 1) * sizeof(char);
+				tmp = (char *) malloc((c = 1) * sizeof(char));
 				if (tmp == NULL)
 					return (NULL);
 				while (start < end)
 					*tmp++ = str[start++];
-					*tmp = '\0';
-					matrix[k] = tmp - c;
-					k++;
-					c = 0;
+				*tmp = '\0';
+				matrix[k] = tmp - c;
+				k++;
+				c = 0;
 			}
 		}
 		else if (c++ == 0)
