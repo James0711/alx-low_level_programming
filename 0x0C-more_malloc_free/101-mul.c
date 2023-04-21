162d6520a4e2b60543206327c916b91de39f90da
James0711 <jengreat94@gmail.com>
2023-04-21 14:24:46 -0700

    multiply two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,47 +1,49 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 #define ERR_MSG "Error"
 
 /**
  * is_digit - A function that checks if a string contains a non-digit char
- * @s: string
- * Return: 0 if a non-digit is found otherwise 1
+ * @s: string to be evaluated
+ *
+ * Return: 0 if a non-digit is found, 1 otherwise
  */
 
 int is_digit(char *s)
 {
-	int a = 0;
+	int i = 0;
 
-	while (s[a])
+	while (s[i])
 	{
-		if (s[a] < '0' || s[a] > '9')
+		if (s[i] < '0' || s[i] > '9')
 			return (0);
-		a++;
+		i++;
 	}
 	return (1);
 }
 
 /**
- * _strlen - A function that returns the length of a string
- * @s: string
- * Return: length of the string
+ * strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
  */
 
 int _strlen(char *s)
 {
-	int r = 0;
+	int i = 0;
 
-	while (s[r] != '\0')
+	while (s[i] != '\0')
 	{
-		r++;
+		i++;
 	}
-	return (r);
+	return (i);
 }
 
 /**
- * errors - A function that ha errors for main
+ * errors - handles errors for main
  */
 
 void errors(void)
@@ -51,29 +53,30 @@ void errors(void)
 }
 
 /**
- * main - A function that multiplies two positive numbers
+ * main - multiplies two positive numbers
  * @argc: number of arguments
  * @argv: array of arguments
- * Return: 0 (success)
+ *
+ * Return: always 0 (Success)
  */
 
 int main(int argc, char *argv[])
 {
-	int len, len1, len2, *result, r = 0, digit1, digit2, a, carry;
 	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
 	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 		errors();
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
-	len =  len1 + len2 + 1;
-	result = malloc(len * sizeof(int));
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
 	if (!result)
 		return (1);
-	for (a = 0; a <= len1 + len2; a++)
-		result[a] = 0;
-	for (len1 = len1 - 1; len1 >= 0; len--)
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
 		digit1 = s1[len1] - '0';
 		carry = 0;
@@ -87,14 +90,14 @@ int main(int argc, char *argv[])
 		if (carry > 0)
 			result[len1 + len2 + 1] += carry;
 	}
-	for (a = 0; a < len - 1; a++)
+	for (i = 0; i < len - 1; i++)
 	{
-		if (result[a])
-			r = 1;
-		if (r)
-			_putchar(result[a] + '0');
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
 	}
-	if (!r)
+	if (!a)
 		_putchar('0');
 	_putchar('\n');
 	free(result);
